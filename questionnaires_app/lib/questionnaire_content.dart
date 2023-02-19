34581d76c669b8b8b5e267e90931624190e95180
Gena Kogan <rgkogan@gmail.com>
2023-02-19 17:03:20 +0200

    routing, add data for each card

--- questionnaires_app/lib/questionnaire_content.dart
+++ questionnaires_app/lib/questionnaire_content.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class QuestionnaireContent extends StatelessWidget {
-  const QuestionnaireContent({super.key});
-
+  /* final String id;
+  final String title;
+  QuestionnaireContent(this.id, this.title); */
+  static const routeName = 'QuestionnaireContent';
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final id = routeArgs['id'];
+    final title = routeArgs['title'];
+    //--------------------------------------
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('TODO'),
+        title: Text(title),
       ),
-      body: Center(
-        child: Text('TODO'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
