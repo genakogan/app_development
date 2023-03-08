4fe4c9dfc25b8d2805187007ba01531b5ac53f17
Genady kogan <rgkogan@gmail.com>
2023-03-09 00:10:52 +0200

    input text in questionnaires screen

--- questionnaires_builder/lib/screens/questionnaires_screen.dart
+++ questionnaires_builder/lib/screens/questionnaires_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
-
+import '../transaction.dart';
 import '../dummy_data.dart';
 import '../widgest/questionnaire_item.dart';
 
 class QuestionnairesScreen extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,18 +26,55 @@ class QuestionnairesScreen extends StatelessWidget {
           'Questionnaires',
         ),
       ),
-      body: GridView(
-          padding: const EdgeInsets.all(25),
-          children: DUMMY_CATEGORIES
-              .map((catData) =>
-                  QuestionnaireItem(catData.id, catData.title, catData.color))
-              .toList(),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  TextButton(
+                    child: Text('Add Transaction'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GridView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(25),
+            children: DUMMY_CATEGORIES
+                .map((catData) =>
+                    QuestionnaireItem(catData.id, catData.title, catData.color))
+                .toList(),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
